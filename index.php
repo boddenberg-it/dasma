a21f365e36827a3dd80000de91b2db124c8ad41e
André Boddenberg <andre@boddenberg.it>
2018-05-13 20:27:28 +0200

    Introduce style.css

--- index.php
+++ index.php
@@ -1,24 +1,48 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html>
+
 <head>
-<title>Datenschutz Boddenberg.it</title>
+    <title>Datenschutz</title>
+    <link rel="stylesheet" href="style.css" type="text/css">
 </head>
+
 <body>
+    <div class="background-image"></div>
+    <div id="header">
+        <div class="wrapper">
+            <img class="logo" src="logo.png"/>
+            <div class="firmenname"><span>COMPANY</span></div>
+        </div>
+    </div>
 
-<img class="logo" src="" alt="LOGO HERE"></img>
+    <div class="container">
+        <div class="outer">
+            <div class="middle">
+                <div class="inner">
 
-<h1>title of company</h1>
-<br>
-Please foooobar......
-</p>
-<br>
-<h2>Register:</h2>
-<br>
-<form action="register.php" method="post">
- <p>mail: <input type="mail" name="mail0" /></p>
- <p>mail: <input type="text" name="mail1" /></p>
- <button type="submit">submit</button>
-</form>
-<br>
+                   <div class="content">
+                    <h1>TEST</h1>
+                    <p>
+                  Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et   justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+                    </p>
+                    </div>
+                    <div class="registerform">
+                        <form action="register.php" method="post">
+                            <label class="signup-resp__form__label signup-resp__form__label--email"></label>
+                            <input placeholder="email" type="mail" name="mail0" />
+                            <label class="signup-resp__form__label signup-resp__form__label--email"></label>
+                            <input placeholder="Repeat email" type="mail" name="mail1" />
+                            <button type="submit">register</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+    </div>
+    <div id="footer">
+        <span>COMPANY 2018 - <a class="footer-link" href="https://">Impressum/Disclaimer</a></span>
+    </div>
 </body>
 </html>
